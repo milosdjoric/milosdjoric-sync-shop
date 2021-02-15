02911b8335d655cd8099f3fd6fe4bcfc1556efa5
Milos Djoric <djoric.inbox@gmail.com>
2021-02-15 19:12:14 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slick-slider-products').slick({
+        infinite: false,
+        slidesToShow: 4.5,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+    });
+});
