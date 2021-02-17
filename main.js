e1e29102317d81999d1ea4fd4ee850dcecd46c68
Milos Djoric <djoric.inbox@gmail.com>
2021-02-17 23:22:57 +0100

    responsiveness fixed

--- main.js
+++ main.js
@@ -5,5 +5,30 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
